672b8b51fa6188766927ca9975709d69aa0f3f90
AlexGoncalvesSilva <89552270+AlexGoncalvesSilva@users.noreply.github.com>
2023-05-19 12:38:25 -0300

    Ajustes Narrativa

--- Assets/Scripts/Controlers/TutorialController.cs
+++ Assets/Scripts/Controlers/TutorialController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class TutorialController : MonoBehaviour
 {
     public Text text;
+    public GameObject textFinal;
 
     public int i = 0;
 
@@ -18,34 +19,62 @@ public class TutorialController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (i <= 4) 
+        textTutorial();
+        finishedTutorial();
+    }
+
+    void textTutorial()
+    {
+        if (i <= 4)
         {
-            PlayerMovement.instance.canMove= false;
+            PlayerMovement.instance.canMove = false;
         }
-        else { PlayerMovement.instance.canMove= true;}
+        else { PlayerMovement.instance.canMove = true; }
         if (Input.GetKeyDown(KeyCode.E))
         {
             i += 1;
             if (i == 1)
             {
-                text.text = "AO FALAR COM OS NPCs UTILIZE O MOUSE PARA SELECIONAR A OPÇÃO DE RESPOSTA QUE VOCÊ DESEJA";
-            }else if (i == 2)
+                text.text = "PARA INTERAGIR COM OBJETOS E NPCs UTILIZE O 'E'";
+            }
+            else if (i == 2)
             {
-                text.text = "UTILIZE 'C' PARA ACESSAR O CELULAR E DEPOIS 'M' PARA ACESSAR O MENU";
+                text.text = "AO FALAR COM OS NPCs UTILIZE O MOUSE PARA SELECIONAR A OPÇÃO DE RESPOSTA QUE VOCÊ DESEJA";
             }
             else if (i == 3)
             {
                 text.text = "PARA ANDAR UTILIZE AS TECLAS 'W, A, S, D'";
             }
-            else if (i == 4) 
-            { 
-                text.text = "AO INTERAGIR COM OBJETOS PEGÁVEIS VOCÊ PODE MEXE-LO UTILIZANDO O MOUSE COM O BOTÃO ESQUERDO PRECIONADO"; 
+            else if (i == 4)
+            {
+                text.text = "UTILIZE 'C' PARA ACESSAR O SEU CELULAR";
             }
-            else if (i>4)
+            else if (i == 5)
+            {
+                text.text = "AO INTERAGIR COM OBJETOS PEGÁVEIS VOCÊ PODE MEXE-LO UTILIZANDO O MOUSE COM O BOTÃO ESQUERDO PRECIONADO";
+            }
+            else if (i > 6)
             {
                 text.text = "";
             }
-            
+
         }
+    }
+    
+    void finishedTutorial()
+    {
+        if (CountPistas.instance.countEvidenciasAnalisadas >= CountPistas.instance.MaxEvidenciasDaCena)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine("rotinaFInal");
+            }
+        }
+    }
+    
+    IEnumerator rotinaFInal()
+    {
+        yield return new WaitForSeconds(3f);
+        textFinal.SetActive(true);
     }
 }
