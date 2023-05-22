7b88985069bcb6b7f6fdfd9603a3b476b3ed4873
Rubens Marreira <rubenslevimarreira@gmail.com>
2023-05-22 15:38:22 -0300

    att

--- Assets/Scripts/Controlers/Buttons/LevelsButtons.cs
+++ Assets/Scripts/Controlers/Buttons/LevelsButtons.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelsButtons : MonoBehaviour
 {
+
+    private CountPistas countPistas;
+    public Button levelUnlockedButton;
+    
+    private void Start()
+    {
+        countPistas = CountPistas.instance;
+        levelUnlockedButton.interactable = CountPistas.allCollectedClues;
+    }
+
     public void LevelButton(int i)
     {
         UiManager.instance.hidePanelLevels();
--- Assets/Scripts/Controlers/Pistas/CountPistas.cs
+++ Assets/Scripts/Controlers/Pistas/CountPistas.cs
@@ -14,6 +14,7 @@ public class CountPistas : MonoBehaviour
     private Interactables interactables;
 
     public string cenaKey; // Chave única para cada cena
+    public static bool allCollectedClues = false;
 
     public static CountPistas instance;
 
@@ -47,6 +48,7 @@ public class CountPistas : MonoBehaviour
         if (countEvidenciasAnalisadas >= MaxEvidenciasDaCena)
         {
             Debug.Log("Todas as pistas foram analisadas!");
+            allCollectedClues = true;
             EnableOutroMenuButton();
         }
     }
