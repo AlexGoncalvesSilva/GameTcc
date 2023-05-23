933aa1dbf013552ac68592e3ea46cdaaf679df26
AlexGoncalvesSilva <89552270+AlexGoncalvesSilva@users.noreply.github.com>
2023-05-23 12:39:36 -0300

    BugFixed

--- Assets/OfficeFeedback.cs
+++ Assets/OfficeFeedback.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Rendering;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
+using static UnityEditor.PlayerSettings;
 using static UnityEngine.Rendering.DebugUI;
 
 public class OfficeFeedback : MonoBehaviour
@@ -19,6 +21,7 @@ public class OfficeFeedback : MonoBehaviour
     public Transform PlayerCamera;
     public bool playerInteractWithNotebook;
     public bool alredyInteract;
+    public bool showText;
     public float MaxDistance = 5;
 
 
@@ -63,32 +66,48 @@ public class OfficeFeedback : MonoBehaviour
             PlayerMovement.instance.canMove = false;
         }
         else { PlayerMovement.instance.canMove = true; }
+      
         if (Input.GetKeyDown(KeyCode.E) && playerInteractWithNotebook == true && alredyInteract == false)
         {
-            i += 1;
-            if (i == 2)
+            StartCoroutine("rotinaText");
+            if (showText == true)
             {
-                text.text = "VAMOS VER... É O CASO DE DANIEL, ELE DESAPARECEU HÁ DOIS DIAS SEM DEIXAR NENHUM RASTRO";
+                i += 1;
+                if (i == 1)
+                {
+                    text.text = "ENVIEI PARA O DEPARTAMENTO O QUE CONSEGUI NA JOALHERIA. AGORA TÁ NA HORA DE FOCAR NO OUTRO CASO";
+                }
+                else if(i == 2)
+                {
+                    text.text = "VAMOS VER... É O CASO DE DANIEL, ELE DESAPARECEU HÁ DOIS DIAS SEM DEIXAR NENHUM RASTRO";
+                }
+                else if (i == 3)
+                {
+                    text.text = "BOM, EU JÁ ORGANIZEI O CASO NO MEU QUADRO";
+                }
+                else if (i == 4)
+                {
+                    text.text = "POR ONDE DEVO COMEÇAR EXATAMENTE? BOM, RECEBI UMA CÓPIA DA CHAVE DA CASA DELE IREI LÁ PRIMEIRO";
+                }
+                else if (i == 5)
+                {
+                    text.text = "DEPOIS IREI NA ONG ONDE ELE TRABALHAVA E LÁ CONVERSAREI COM A ANA.";
+                }
+                else if (i == 6)
+                {
+                    text.text = "";
+                    playerInteractWithNotebook = false;
+                    alredyInteract = true;
+                }
             }
-            else if (i == 3)
-            {
-                text.text = "BOM, EU JÁ ORGANIZEI O CASO NO MEU QUADRO";
-            }
-            else if (i == 4)
-            {
-                text.text = "POR ONDE DEVO COMEÇAR EXATAMENTE? BOM, RECEBI UMA CÓPIA DA CHAVE DA CASA DELE IREI LÁ PRIMEIRO";
-            }
-            else if (i == 5)
-            {
-                text.text = "DEPOIS IREI NA ONG ONDE ELE TRABALHAVA E LÁ CONVERSAREI COM A ANA.";
-            }
-            else if (i == 6)
-            {
-                text.text = "";
-                playerInteractWithNotebook = false;
-                alredyInteract = true;
-            }
+            
         }
+    }
+
+    IEnumerator rotinaText()
+    {
+        yield return new WaitForSeconds(1f);
+        showText = true;
     }
 
 }
