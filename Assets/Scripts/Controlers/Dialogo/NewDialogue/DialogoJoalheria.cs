649ce9a2a8d67a33c1e6012b9889b29d616c2262
AlexGoncalvesSilva <89552270+AlexGoncalvesSilva@users.noreply.github.com>
2023-05-24 04:38:52 -0300

    Dialogo BugFixed

---
+++ Assets/Scripts/Controlers/Dialogo/NewDialogue/DialogoJoalheria.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogoJoalheria : MonoBehaviour
+{
+    public FalaNPC[] falas = new FalaNPC[2];
+
+    private bool dialogoConcluido = false;
+
+    public UnityEvent IsTalking;
+    public UnityEvent OnFinishTalking;
+
+    public bool playerIntetact;
+
+    public Transform PlayerCamera;
+    public float MaxDistance = 5;
+
+    public static DialogoJoalheria instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInteraction();
+    }
+
+    void playerInteraction()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIntetact == false)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, MaxDistance))
+            {
+                if (hit.transform.tag == "NPCJoalheria")
+                {
+                    playerIntetact = true;
+                    playerDioalogo();
+                }
+            }
+            else
+            {
+                playerIntetact = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    void playerDioalogo()
+    {
+        if (playerIntetact)
+        {
+            IsTalking.Invoke();
+            CameraController.instance.CantMoveCamera();
+
+            if (!dialogoConcluido)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+                DialogoController.instance.ProximaFala(falas[0]);
+                Analyze.instance.PistaDialogo();
+            }
+            else
+            {
+                DialogoController.instance.ProximaFala(falas[1]);
+                CameraController.instance.CanMoveCamera();
+            }
+
+            dialogoConcluido = true;
+        }
+    }
+
+    public void finished()
+    {
+        StartCoroutine("RotinaInteract");
+    }
+
+    IEnumerator RotinaInteract()
+    {
+        yield return new WaitForSeconds(3f);
+        playerIntetact = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+
+        }
+
+    }
+}
