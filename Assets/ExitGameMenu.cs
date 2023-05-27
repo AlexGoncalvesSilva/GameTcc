372634dbba27eaea5eec2bb85d46817be3d81de3
Rubens Marreira <rubenslevimarreira@gmail.com>
2023-05-27 20:33:37 -0300

    exit game button

---
+++ Assets/ExitGameMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGameMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
